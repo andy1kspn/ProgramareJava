c806c3965e1dfb1ca16f61676bc4e85756473ea8
danielagh14 <dgheletchi@gmail.com>
2023-03-28 19:17:06 +0300

    Commit 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,48 @@
 package org.example;
 
+
 public class Main {
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello Andy!");
+        int i = 10;
+
+        if(i>10){
+            System.out.println("Hello World!");
+        }
+
+
+
+        do{
+            System.out.println("Hello World!");
+
+        }
+        while (i>10);
+
+
+        while (i>10){
+            System.out.println("Hello World!");
+        }
+
+        for (int j=0;j>10;j++){
+            System.out.println("Hello World!");
+        }
+
+
+        int y = 5;
+        int u = 6;
+
+
+        System.out.println(y++);
+        System.out.println(++u);
+
+
 
     }
 }
+
+
+
+
+
